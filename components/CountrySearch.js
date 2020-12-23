25e38028cc834392326104b4cd84b5083b5130b8
Angle-Brackets <superstarmario12@gmail.com>
2020-12-22 20:01:12 -0500

    Fixed minor error in CountrySearch.js

--- components/CountrySearch.js
+++ components/CountrySearch.js
@@ -5,8 +5,8 @@ const {Search} = Input;
 
 const CountrySearch = () => {
     return (
-        <Search placeholder="Unavailable" onSearch={value => console.log(value)} enterButton style={{position: "absolute", right: "2%", width: 200, marginTop: "1rem", backgroundColor: "white"}}
-        );
+        <Search placeholder="Unavailable" onSearch={value => console.log(value)} enterButton style={{position: "absolute", right: "2%", width: 200, marginTop: "1rem", backgroundColor: "white"}}/>
+    );
 }
 
 
